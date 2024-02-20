74f234fbd898aebcdf3c957f2013d98fed3a8fad
roudayna <roudaynacherif9@gmail.com>
2024-02-20 11:45:32 +0100

    commit1 tp3

---
+++ Tp3/Controllers/ProduitController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tp3.Models;
+
+namespace Tp3.Controllers
+{
+    public class ProduitController : Controller
+    {
+        // GET: Produit
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Hello to the home page of TP3";
+
+            return View();
+        }
+
+        public ActionResult DisplayOneProduct()
+        {
+            Produit produit = new Produit()
+            {
+                Id = 1,
+                Name = "Produit 1",
+                Prix = 10
+            };
+            ViewBag.Produit = produit;
+
+            return View();  
+        }
+
+        public ActionResult DisplayManyproducts()
+        {
+            List<Produit> produits = new List<Produit>
+            {
+                new Produit { Id = 1, Name = "Produit 1", Prix = 10 },
+                new Produit { Id = 2, Name = "Produit 2", Prix = 20 },
+                new Produit { Id = 3, Name = "Produit 3", Prix = 30 }
+            };
+
+            ViewBag.Produits = produits;
+
+
+            return View();
+        }
+
+    }
+}
---
+++ Tp3/Models/Produit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tp3.Models
+{
+    public class Produit
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Prix { get; set; }
+    }
+}
